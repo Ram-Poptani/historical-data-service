a4ffa3da1432cdf1de0080cd82f8d369d2a34690
Ram Poptani <911S205126@adani.com>
2026-01-24 16:52:26 +0530

    INIT

---
+++ src/main/java/org/binance/pastdataservice/PastDataServiceApplication.java
@@ -0,0 +1,13 @@
+package org.binance.pastdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/binance/pastdataservice/PastDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binance.pastdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
