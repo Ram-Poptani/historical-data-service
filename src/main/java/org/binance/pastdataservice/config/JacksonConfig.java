9698f052c769adefc044b8fd9863582af46268d9
Ram Poptani <911S205126@adani.com>
2026-01-25 15:41:12 +0530

    Refactor: Updated JacksonConfig

--- src/main/java/org/binance/pastdataservice/config/JacksonConfig.java
+++ src/main/java/org/binance/pastdataservice/config/JacksonConfig.java
@@ -1,16 +1,20 @@
 package org.binance.pastdataservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
     }
 
     @Bean
